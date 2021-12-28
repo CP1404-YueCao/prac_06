4b856dfaf20b505d75e714b557e75efca6fdf7f6
Yue Cao <yue.cao@my.jcu.edu.au>
2021-12-28 21:53:32 +0800

    Add the main function to display details of guitars

---
+++ guitars.py
@@ -0,0 +1,14 @@
+from guitar import Guitar
+
+def main():
+    """Guitar program"""
+    guitars = []
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(guitar_to_add, "added.")
+        name = input("Name: ")
