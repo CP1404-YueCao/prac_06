96f8714e4a008c23b74e76a7e177b03186135b87
Yue Cao <yue.cao@my.jcu.edu.au>
2021-12-28 21:16:18 +0800

    Add __init__ function to construct a ProgrammingLanguage

--- programming_language.py
+++ programming_language.py
@@ -1,4 +1,11 @@
 class ProgrammingLanguage:
     """Display information about a programming language"""
 
+    def __init__(self, name, typing, reflection, year):
+        """Construct a ProgrammingLanguage"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
     
