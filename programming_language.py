511a36a9e4c80bac4651081eddfa3e3ea59722b4
Yue Cao <yue.cao@my.jcu.edu.au>
2021-12-28 21:36:04 +0800

    Add a funcion to run a simple tests

--- programming_language.py
+++ programming_language.py
@@ -15,4 +15,19 @@ class ProgrammingLanguage:
     def is_dynamic(self):
         """Determine if language is dynamically typed"""
         return self.typing == "Dynamic"
-    
+
+    def run_tests():
+        """Run tests on ProgrammingLanguage class"""
+        ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+        python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+        visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+        languages = [ruby, python, visual_basic]
+        print(ruby)
+        print("The dynamically typed languages are: ")
+        for language in languages:
+            if language.is_dynamic():
+                print(language.name)
+
+
+    if __name__ == "__main__":
+        run_tests()
